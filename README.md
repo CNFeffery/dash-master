0f79760efb491bd7a2a921eefc71f30ad709bd83
CNFeffery <fefferypzy@gmail.com>
2023-06-23 08:55:31 +0800

    新增文章：Dash应用页面整体布局技巧

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 
 - [Dash 2.9.0版本重磅新功能一览](https://www.cnblogs.com/feffery/p/17227380.html) 　:airplane:[仓库路径](./历史文章/Dash2.9.0版本重磅新功能一览)
 - [10分钟极速入门dash应用开发](https://www.cnblogs.com/feffery/p/17333077.html) 　:airplane:[仓库路径](./历史文章/10分钟极速入门dash应用开发)
+- [Dash应用页面整体布局技巧](https://www.cnblogs.com/feffery/p/17498581.html) 　:airplane:[仓库路径](./历史文章/Dash应用页面整体布局技巧)
---
+++ 历史文章/Dash应用页面整体布局技巧/demo-apps/app1.py
@@ -0,0 +1,104 @@
+import dash
+from dash import html
+import feffery_antd_components as fac
+import feffery_utils_components as fuc
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(
+    [
+        fuc.FefferyDiv(
+            [
+                # 利用fac中的网格系统进行页首元素的排布
+                fac.AntdRow(
+                    [
+                        # logo+标题
+                        fac.AntdCol(
+                            fac.AntdSpace(
+                                [
+                                    fac.AntdImage(
+                                        src=dash.get_asset_url(
+                                            './imgs/logo.png'
+                                        ),
+                                        height=54,
+                                        preview=False
+                                    ),
+                                    fac.AntdText(
+                                        'XX数据平台',
+                                        strong=True,
+                                        style={
+                                            'fontSize': 32
+                                        }
+                                    )
+                                ]
+                            ),
+                            flex='none',
+                            style={
+                                'height': 'auto'
+                            }
+                        ),
+
+                        # 菜单栏
+                        fac.AntdCol(
+                            fac.AntdMenu(
+                                menuItems=[
+                                    {
+                                        'component': 'Item',
+                                        'props': {
+                                            'key': f'模块{i}',
+                                            'title': f'模块{i}',
+                                        }
+                                    }
+                                    for i in range(1, 6)
+                                ],
+                                defaultSelectedKey='模块1',
+                                mode='horizontal',
+                                style={
+                                    'maxWidth': 600,
+                                    'borderBottom': 'none'
+                                }
+                            ),
+                            flex='none',
+                            style={
+                                'height': 'auto'
+                            }
+                        )
+                    ],
+                    align='middle',
+                    justify='space-between',
+                    wrap=False,  # 阻止自动换行
+                    style={
+                        'height': '100%'
+                    }
+                )
+            ],
+            style={
+                'height': 64,
+                'padding': '0 25px',
+                'background': 'white',
+                'borderBottom': '1px solid #f0f0f0'
+            }
+        ),
+
+        # 内容区
+        html.Div(
+            fuc.FefferyDiv(
+                '示例内容'*2000,
+                shadow='always-shadow-light',
+                style={
+                    'borderRadius': 8,
+                    'background': 'white',
+                    'padding': 24
+                }
+            ),
+            style={
+                'minHeight': 'calc(100vh - 64px)',
+                'background': '#f0f2f5',
+                'padding': 50
+            }
+        )
+    ]
+)
+
+if __name__ == '__main__':
+    app.run(debug=True)
